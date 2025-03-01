046745d16f7fb10e831ebedaeb5db66c421eb34f
Rui Campos <ruigabriel2@ua.pt>
2025-03-01 11:20:08 +0000

    Initial Commit

---
+++ Main/SimulPar.java
@@ -0,0 +1,24 @@
+package Main;
+
+/**
+ *    Definition of the simulation parameters.
+ */
+public final class SimulPar
+{
+    /**
+     * Minimum of voters.
+     */
+    public static final int MIN_NUMBER_OF_VOTERS = 3;
+
+    /**
+     * Maximum of voters.
+     */
+    public static final int MAX_NUMBER_OF_VOTERS = 10;
+
+
+    /**
+     *   It can not be instantiated.
+     */
+    private SimulPar ()
+    { }
+}
